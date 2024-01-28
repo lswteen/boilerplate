1211600cf636ce4eb6262a2c1c432ec3fcdca517
renzo.1980 <renzo.1980@jobkorea.co.kr>
2024-01-28 10:19:01 +0900

    [main] add :.gitignore

---
+++ src/main/java/com/renzo/zelda/ZeldaApplication.java
@@ -0,0 +1,13 @@
+package com.renzo.zelda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeldaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeldaApplication.class, args);
+	}
+
+}
